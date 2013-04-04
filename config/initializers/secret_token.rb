24d386917aa9af771cc3cac9c0cd479c64bfc378
Dave Guarino <daguar@gmail.com>
2013-04-03 23:01:15 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Open311ApiServer::Application.config.secret_token = '7206b042524370c7a0d2b58a5369b73eed71ff6dbd43201e7840bd0dc69e4d8d84a6afdec573dfcaabe6755f1ac51bb159e3f6ce02969dfaf5a124dfe8d0e7a4'
