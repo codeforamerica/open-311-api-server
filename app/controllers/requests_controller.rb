2177738ff28cb0cf3ac4fbfad1c5aa20a3d2b000
Dave Guarino <dave@codeforamerica.org>
2013-04-16 09:59:59 -0700

    Have show action search by service_request_id

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -13,7 +13,7 @@ class RequestsController < ApplicationController
   # GET /requests/1
   # GET /requests/1.json
   def show
-    @request = Request.find(params[:id])
+    @request = Request.find_by_service_request_id(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
