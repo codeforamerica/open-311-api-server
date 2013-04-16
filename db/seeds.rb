6dcd3abf3c4910e92ed2e280cb2e27479b8af211
Dave Guarino <dave@codeforamerica.org>
2013-04-16 10:22:29 -0700

    Modify db/seeds.rb to include Kris test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Request.create! service_request_id: "1234", status: "open", service_name: "garbage", service_code: "GARB", description: "someone left shit on my lawn", requested_datetime: "2013-04-01 10:48".to_datetime
+Request.create! service_request_id: "1234", status: "open", service_name: "garbage", service_code: "GARB", \
+  description: "someone left shit on my lawn", requested_datetime: "2013-04-01 10:48".to_datetime
+Request.create! service_request_id: "493278", status: "3", service_name: "test", service_code: "TEST", \
+  description: "", requested_datetime: "2013-04-11 12:33".to_datetime
+
