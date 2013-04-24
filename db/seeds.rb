e7e42518a504dd4c68d084d1876430eba73dd7e7
Dave Guarino <dave@codeforamerica.org>
2013-04-24 10:30:00 -0700

    Comment out seeds (which were for exploratory testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@
 #  description: "", requested_datetime: "2013-04-11 12:33".to_datetime
 #Request.create! service_request_id: "49327899999", status: "closed", service_name: "test", service_code: "TEST", \
 #  description: "", requested_datetime: "2013-04-11 12:33".to_datetime
-@r = Request.create service_request_id: "2323", status: "open", service_name: "garbage", service_code: "GARB", \
-  description: "ticket with notes", requested_datetime: "2013-04-01 10:48".to_datetime
-@r.notes.create datetime: "2013-04-01 10:48".to_datetime, type: "activity", summary: "Sent to AEP"
+#@r = Request.create service_request_id: "2323", status: "open", service_name: "garbage", service_code: "GARB", \
+#  description: "ticket with notes", requested_datetime: "2013-04-01 10:48".to_datetime
+#@r.notes.create datetime: "2013-04-01 10:48".to_datetime, type: "activity", summary: "Sent to AEP"
 
